e3ed9608371e6face0405fc4e6a37f8fea0bf39e
ovibinzia1885 <nazmul35-1885@diu.edu.bd>
2020-11-16 18:09:17 +0600

    final year project

--- finalyear/public/urls.py
+++ finalyear/public/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
 
     path('',views.publicindex,name="publicindex"),
     path('addproblem/', views.addproblem, name="addproblem"),
+    path('applyapplication/', views.applyapplication, name="applyapplication"),
+    path('applylicence/', views.applylicence, name="applylicence"),
+    path('onlinebirthcertificate/', views.onlinebirthcertificate, name="onlinebirthcertificate"),
+    path('GivenHomeTax/', views.GivenHomeTax, name="GivenHomeTax"),
+    path('FeedBack/', views.FeedBack, name="FeedBack"),
 
 
 ]
--- finalyear/public/views.py
+++ finalyear/public/views.py
@@ -5,3 +5,13 @@ def publicindex(request):
 
 def addproblem(request):
     return render(request,'public/addproblem.html')
+def applyapplication(request):
+    return render(request,'public/applyapplication.html')
+def applylicence(request):
+    return render(request,'public/applylicence.html')
+def onlinebirthcertificate(request):
+    return render(request,'public/onlinebirthcertificate.html')
+def GivenHomeTax(request):
+    return render(request,'public/giventax.html')
+def FeedBack(request):
+    return render(request,'public/feedback.html')
