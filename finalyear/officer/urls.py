2119520b22ce6c524e1e6260aabbeec708824734
ovibinzia1885 <nazmul35-1885@diu.edu.bd>
2020-11-17 09:37:14 +0600

    final year project

--- finalyear/officer/urls.py
+++ finalyear/officer/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('officer/',views.officerindex,name="officrindex")
+    path('councilordetlis/',views.councilordetlis,name="councilordetlis"),
+    path('taxview/', views.taxview, name="taxview"),
+    path('viewcertificate/', views.viewcertificate, name="viewcertificate"),
+    path('licenceview/', views.licenceview, name="licenceview"),
+    path('viewproblem/', views.viewproblem, name="viewproblem"),
+    path('workshop/', views.workshop, name="workshop"),
+
 
 ]
--- finalyear/officer/views.py
+++ finalyear/officer/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 def officerindex(request):
     return  render(request,'officer/officerindex.html')
+
+def councilordetlis(request):
+    return render(request,'officer/councilordetlies.html')
+def taxview(request):
+    return render(request,'officer/taxcheek.html')
+def viewcertificate(request):
+    return render(request,'officer/viewbirthcertificate.html')
+def licenceview(request):
+    return render(request,'officer/viewlicencen.html')
+def viewproblem(request):
+    return render(request,'officer/viewproblem.html')
+def workshop(request):
+    return render(request,'officer/workshop.html')
